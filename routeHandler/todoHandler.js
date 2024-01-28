cb848667bcba276d380bd102f9f1866e83b881d0
Saif-Rahman-801 <saifurs102@gmail.com>
2024-01-28 12:44:16 +0600

    put method created

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { default: mongoose } = require("mongoose");
 const Todo = require("../schemas/todoSchema");
 
 // const Todo = new mongoose.model("Todo", todoSchema);
@@ -11,35 +10,22 @@ router.post("/", async (req, res) => {
     const { title, description, status } = req.body;
     // const todo = await Todo.create({title, description, status});
     // res.status(201).json(todo);
-    const todo = new Todo({ title, description, status })
+    const todo = new Todo({ title, description, status });
     const result = await todo.save();
     res.status(201).json(result);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 
-  /* const newTodo = new Todo(req.body); //validate your data with your schema
-
-  await newTodo.save((err) => {
-    if (err) {
-      res.status(500).json({
-        error: "Server side schema validation error",
-      });
-    } else {
-      res.status(500).json({
-        message: "Data insertion successful",
-      });
-    }
-  }); */
 });
 
 // post multiple todo
 router.post("/all", async (req, res) => {
-  try{
+  try {
     const multipleDataArray = req.body;
     const result = await Todo.insertMany(multipleDataArray);
     res.status(201).json(result);
-  } catch(err){
+  } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
@@ -51,7 +37,15 @@ router.get("/", async (req, res) => {});
 router.get("/:id", async (req, res) => {});
 
 // update todo
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  try{
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!todo) return res.status(404).json({ message: "Todo not found" });
+    res.json(todo);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 // delete todo
 router.delete("/:id", async (req, res) => {});
